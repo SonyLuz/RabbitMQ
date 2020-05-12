42783691b09d1ff99265e3c718d811f5ae740463
Sony Luz <sonyluz@bne.com.br>
2020-05-12 11:49:33 -0300

    ajustes

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,81 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Consumer
+{
+    class Program
+    {
+        public static IConnection _connection;
+        public static IModel _channel;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                #region Definindo conexão com o rabbit
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.UserName = "rabbitmq";
+                factory.Password = "rabbitmq";
+                factory.VirtualHost = "development";
+                factory.HostName = "localhost";
+                factory.Port = 5672;
+                factory.ContinuationTimeout = TimeSpan.FromMinutes(1);
+                factory.RequestedHeartbeat = TimeSpan.FromSeconds(300);
+                #endregion
+
+                //Criando conexão
+                _connection = factory.CreateConnection();
+
+                //Criando um canal para para envio e leitura de mensagem
+                _channel = _connection.CreateModel();
+                var queueName = "Minha_Queue";
+
+                ConsumerMsg(queueName);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                //Fechando canal
+                _channel.Close();
+
+                //fechando conexão
+                _connection.Close();
+            }
+
+        }
+
+        public static void ConsumerMsg(string queueName)
+        {
+            bool ready = true;
+            Console.WriteLine("Iniciando Consumo da fila");
+            int count = 0;
+            while (ready)
+            {
+                //get msg
+                var model = _channel.BasicGet(queueName, false);
+
+                if (model == null)
+                {
+                    Thread.Sleep(5000);
+                }
+                else
+                {
+                    count++;
+                    //get msg array bytes
+                    string msg = Encoding.UTF8.GetString(model.Body.ToArray());
+                    Console.WriteLine($"{count}- Mensagem recuperada: {msg}");
+
+                    // executa ack para remoção da msg da fila
+                    _channel.BasicAck(model.DeliveryTag, false);
+                }
+            }
+
+        }
+    }
+}
---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,73 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQ.Publish
+{
+    class Program
+    {
+        public static IConnection _connection;
+        public static IModel _channel;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                #region Definindo conexão com o rabbit
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.UserName = "rabbitmq";
+                factory.Password = "rabbitmq";
+                factory.VirtualHost = "development";
+                factory.HostName = "localhost";
+                factory.Port = 5672;
+                factory.ContinuationTimeout = TimeSpan.FromMinutes(1);
+                factory.RequestedHeartbeat = TimeSpan.FromSeconds(300);
+                #endregion
+
+                //Criando conexão
+                _connection = factory.CreateConnection();
+
+                //Criando um canal para para envio e leitura de mensagem
+                _channel = _connection.CreateModel();
+
+                var exchangeName = "Minha_Exchange";
+                var queueName = "Minha_Queue";
+
+                _channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+                _channel.QueueDeclare(queueName, false, false, false, null);
+                _channel.QueueBind(queueName, exchangeName, string.Empty, null);
+
+                Console.WriteLine("Enviando msg");
+                for (int i = 1; i <= 100; i++)
+                {
+                    //Enviando msg
+                    PublishMsg(exchangeName, $"{i} - Olá Mundo!");
+                    Thread.Sleep(2000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+            finally
+            {
+                //Fechando canal
+                _channel.Close();
+
+                //fechando conexão
+                _connection.Close();
+            }
+        }
+
+        public static void PublishMsg(string exchangeName, string msg)
+        {       
+            //Enviando msg
+            Console.WriteLine(msg);
+            byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes(msg);
+            _channel.BasicPublish(exchangeName, string.Empty, null, messageBodyBytes);
+            
+        }
+    }
+}
